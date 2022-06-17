8eee762da9451f54515278e63e371f5b3ee0c503
yonib98 <battat@campus.technion.ac.il>
2022-06-17 14:21:13 +0000

    finished second part2

--- part1.c
+++ part1.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 int main(int argc,char *argv[]){
 	//char* progname= argv[1];
-	char* progname = "a.out";
+	//char* func_to_debug = argv[2];
+	char* progname = "test.out";
+	char* func_to_debug = "foo";
 	FILE* prog = fopen(progname,"r");
 	if(prog==NULL){
 		printf("couldn't open file\n");
@@ -18,6 +21,54 @@ int main(int argc,char *argv[]){
 		fclose(prog);
 		return 0;
 	}
-	return 0;
 	//------------------------PART2-----------------------
+	fseek(prog,header.e_shoff,SEEK_SET); //Start of section headers table
+	int sh_size = header.e_shentsize;
+	int sh_count = header.e_shnum;
+
+	Elf64_Shdr* sh_arr = malloc(sizeof(Elf64_Shdr)*sh_count);
+	if(sh_arr==NULL){
+		exit(1);
+	}
+	fread(sh_arr,sh_size,sh_count,prog);
+
+
+	Elf64_Shdr sh_symtab;
+	Elf64_Shdr sh_strtab;
+	for(int i=0;i<sh_count;i++){
+		if(sh_arr[i].sh_type==0x2){
+			sh_symtab=sh_arr[i];
+		}
+		if(sh_arr[i].sh_type==0x3 && i!=header.e_shstrndx){
+			sh_strtab=sh_arr[i];
+		}
+	}
+	fseek(prog,sh_strtab.sh_offset,SEEK_SET);
+	char * strtab_dump = malloc(sizeof(char)*sh_strtab.sh_size);
+	fread(strtab_dump,sh_strtab.sh_size,1,prog);
+
+	fseek(prog,sh_symtab.sh_offset,SEEK_SET);
+	int sym_count = sh_symtab.sh_size/sh_symtab.sh_entsize;
+	Elf64_Sym* symtab_entries = malloc(sizeof(Elf64_Sym)*sym_count);
+	if(symtab_entries==NULL){
+		exit(1);
+	}
+	fread(symtab_entries,sh_symtab.sh_entsize,sym_count,prog);
+	bool found_function=false;
+	for(int i=0;i<sym_count;i++){
+		if(i==51){
+			printf("hey");
+		}
+		int strtab_index = symtab_entries[i].st_name;
+		char* sym_name = &strtab_dump[strtab_index];
+		if(strcmp(sym_name,func_to_debug)==0){
+			found_function =true;
+			break;
+		}
+	}
+	if(!found_function){
+		printf("PRF:: %s not found!\n", func_to_debug);
+	}
+
+
 }
---
+++ test.c
@@ -0,0 +1,6 @@
+int foo(int x){
+    return 1;
+}
+int main(){
+    return 0;
+}
